854eb16e326562bc5f5fe6c3dd35c8252a2b7b08
tuannda1503 <ndatuancntt@gmail.com>
2022-09-10 10:37:00 +0700

    update UI betting popup

--- src/app/components/match/betting.match.tsx
+++ src/app/components/match/betting.match.tsx
@@ -1,8 +1,8 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import styles from "../../../styles/fighting-match.module.css";
-import {Col, Row} from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import Spinner from 'react-bootstrap/Spinner';
-import {IMatchLog} from "../../interfaces/match-log.interface";
+import { IMatchLog } from "../../interfaces/match-log.interface";
 import HeroTurn from "../hero/hero-select";
 import Countdown from "react-countdown";
 import {
@@ -15,11 +15,12 @@ import {
   MDBModalHeader,
   MDBModalTitle
 } from "mdb-react-ui-kit";
-import {currentBet, IBetState, placeBet} from "../../http/store/reducers/bet.slice";
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../http/store";
+import { currentBet, IBetState, placeBet } from "../../http/store/reducers/bet.slice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../http/store";
+import Form from 'react-bootstrap/Form';
 
-const BettingMatch = ({id, items, start_time}: { id: number, items: any, start_time: number }) => {
+const BettingMatch = ({ id, items, start_time }: { id: number, items: any, start_time: number }) => {
   const bet: IBetState = useSelector((state: RootState) => state.bet);
   const auth = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch();
@@ -60,13 +61,13 @@ const BettingMatch = ({id, items, start_time}: { id: number, items: any, start_t
 
   const handleBet = (hero_id: number | undefined) => {
     dispatch({
-        type: placeBet.type,
-        payload: {
-          match_id: id,
-          hero_id: hero_id,
-          balance: balance,
-        }
+      type: placeBet.type,
+      payload: {
+        match_id: id,
+        hero_id: hero_id,
+        balance: balance,
       }
+    }
     )
   }
 
@@ -74,15 +75,15 @@ const BettingMatch = ({id, items, start_time}: { id: number, items: any, start_t
     <div>
       <Row className="text-light">
         <Col xs="6">
-          BET TIME: <Countdown date={start_time}/>
+          BET TIME: <Countdown date={start_time} />
         </Col>
       </Row>
       <Row className={styles.card + " justify-content-md-center"}>
         <Col md="6">
-          {home ? <HeroTurn hero={home}/> : <Spinner variant="light" animation="border"/>}
+          {home ? <HeroTurn hero={home} /> : <Spinner variant="light" animation="border" />}
         </Col>
         <Col md="6">
-          {away ? <HeroTurn hero={away}/> : <Spinner variant="light" animation="border"/>}
+          {away ? <HeroTurn hero={away} /> : <Spinner variant="light" animation="border" />}
         </Col>
       </Row>
       {
@@ -100,12 +101,49 @@ const BettingMatch = ({id, items, start_time}: { id: number, items: any, start_t
             </MDBModalHeader>
             <MDBModalBody>
               <form>
-                <MDBInput onChange={(e) => setBalance(parseInt(e.target.value))}
-                          className='mb-4' defaultValue={1000} type='number' label='Gold for bet'/>
+                <MDBInput onChange={(e) => {
+                  setBalance(e.target.value === '' ? 0 : parseInt((e.target.value).replaceAll(',', '')))
+                }}
+                  className='mb-4' value={Intl.NumberFormat().format(balance)} type='text' label='Gold for bet' />
+                <Form.Label>Remaining: ${Intl.NumberFormat().format(auth.balance - balance)}</Form.Label>
+                <Form.Range color="dark" value={balance} min={0} max={auth.balance} onChange={(e) => setBalance(parseInt(e.target.value))} />
                 <Row>
+                  <Col className='xs-2'>
+                    <MDBBtn block color="dark" type='button' outline={balance === auth.balance * 0.05 ? false : true}
+                      onClick={() => setBalance(auth.balance * 0.05)}
+                    >
+                      5%
+                    </MDBBtn>
+                  </Col>
+                  <Col className='xs-2'>
+                    <MDBBtn block color="dark" type='button' outline={balance === auth.balance * 0.25 ? false : true}
+                      onClick={() => setBalance(auth.balance * 0.25)}>
+                      25%
+                    </MDBBtn>
+                  </Col>
+                  <Col className='xs-2'>
+                    <MDBBtn block color="dark" type='button' outline={balance === auth.balance * 0.5 ? false : true}
+                      onClick={() => setBalance(auth.balance * 0.5)}>
+                      50%
+                    </MDBBtn>
+                  </Col>
+                  <Col className='xs-2'>
+                    <MDBBtn block color="dark" type='button' outline={balance === auth.balance * 0.75 ? false : true}
+                      onClick={() => setBalance(auth.balance * 0.75)}>
+                      75%
+                    </MDBBtn>
+                  </Col>
+                  <Col className='xs-2'>
+                    <MDBBtn block color="dark" type='button' outline={balance === auth.balance ? false : true}
+                      onClick={() => setBalance(auth.balance)}>
+                      100%
+                    </MDBBtn>
+                  </Col>
+                </Row>
+                <Row className='mt-5'>
                   <Col className='xs-6'>
                     <MDBBtn block outline color="success" type='button'
-                            onClick={() => handleBet(home?.id)}
+                      onClick={() => handleBet(home?.id)}
                     >
                       {home?.name}
                     </MDBBtn>
